258a994dd18b1f0f7392841e6c31deaac1c4ccac
代秀磊 <daixiulei@outlook.com>
2022-12-06 21:51:10 +0800

    feat: 修改生成目录

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/*
+ * @Author: xiulei.dai
+ * @Date: 2022-12-05 09:48:40
+ * @LastEditors: xiulei.dai
+ * @LastEditTime: 2022-12-06 21:50:22
+ * @Description:
+ */
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [react()],
+    build: {
+        outDir: "./docs"
+    }
 })
